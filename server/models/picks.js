b3a0548b1b7d51d275a1beb2dfb9c8033feba1ef
Gabe Baduqui <baduquig@gmail.com>
2023-01-04 19:23:13 -0700

    condense data model to single Picks collection

--- server/models/picks.js
+++ server/models/picks.js
@@ -1,18 +1,39 @@
 import mongoose from "mongoose";
 
 const pickSchema = mongoose.Schema({    
+    // Pick related data
     _id: {
         userId: Number,
         gameId: Number
     },
-    schoolPicked: Number,
-    scoreDifferential: String,
+    userName: String,
+    schoolPicked: {
+        type: Number,
+        default: ''
+    },
+    scoreDifferential: {
+        type: String,
+        default: ''
+    },
     points: {
         type: String,
         default: 0
     },
     createdDateTime: Timestamp,
-    lastUpdatedDateTime: Timestamp
+    lastUpdatedDateTime: Timestamp,
+
+    // Game related data
+    week: Number,
+    day: Number,
+    date: String,
+    gameTime: Timestamp,
+    finalScore: String,
+    awaySchoolId: Number,
+    awaySchoolName: String,
+    awaySchoolMascot: String,
+    homeSchoolId: Number,
+    homeSchoolName: String,
+    homeSchoolMascot: String
 });
 
 
